1b3b3b61430d768bd01446967d225ebc043def5f
hnewey7 <hnewey7@gmail.com>
2025-05-30 11:22:08 +0100

    feat: add InventorManager class
    - add method for connecting to Inventor.

---
+++ tests/InventorManager_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.InventorManager import InventorManager
+
+# - - - - - - - - - - - - - - - - - 
+
+def test_connection():
+    """
+    Test connection to Inventor application.
+    """
+    manager = InventorManager()
+    assert manager.app is not None
