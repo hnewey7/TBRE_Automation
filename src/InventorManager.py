7c01b8f4abfbe80a8e8d28b6271f4d9b15711a94
hnewey7 <hnewey7@gmail.com>
2025-06-03 16:48:56 +0100

    feat: add select document method
    - add select document method.
    - add test for select document method.
    - add sleep after disconnect.

--- src/InventorManager.py
+++ src/InventorManager.py
@@ -4,6 +4,9 @@ import logging.config
 import os
 from datetime import datetime
 import json
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import time
 
 # - - - - - - - - - - - - - - - - - - - - -
 
@@ -15,12 +18,25 @@ logger = logging.getLogger()
 
 class InventorManager:
     def __init__(self):
+        """
+        A class for managing connection to Inventor application.
+
+        Attributes
+        ----------
+        app : win32com.client.Dispatch
+            Inventor Application
+        """
+        self.app = None  # Inventor Application
+
         # Connect to Inventor application.
         ret = self.connect_to_inventor()
         if not ret:
             logger.error("Failed to connect to Inventor application.")
         else:
             logger.info("Successfully connected to Inventor application.")
+
+    # - - - - - - - - - - - - - - - -
+    # Methods for managing Inventor application connection
 
     def connect_to_inventor(self):
         """
@@ -32,13 +48,15 @@ class InventorManager:
         try:
             self.app = win32com.client.Dispatch("Inventor.Application")
             self.app.Visible = True
-            CastTo = gencache.EnsureModule("{D98A091D-3A0F-4C3E-B36E-61F62068D488}", 0, 1, 0)
+            CastTo = gencache.EnsureModule(
+                "{D98A091D-3A0F-4C3E-B36E-61F62068D488}", 0, 1, 0
+            )
             self.app = CastTo.Application(self.app)
             return True
         except Exception as e:
             logger.error(f"Error connecting to Inventor: {e}")
             return False
-    
+
     def disconnect(self):
         """
         Disconnect from the Inventor application.
@@ -46,10 +64,41 @@ class InventorManager:
         try:
             if self.app:
                 self.app.Quit()
+                time.sleep(1)
                 logger.info("Disconnected from Inventor application.")
                 self.app = None
         except Exception as e:
             logger.error(f"Error disconnecting from Inventor: {e}")
+
+    # - - - - - - - - - - - - - - - -
+    # Methods for selecting documents.
+
+    def select_document(self, filename: str = None):
+        """
+        Select a document by prompting user to file.
+
+        Args:
+            filename (str): Filename, optional
+
+        Returns:
+            bool: True if the document is found and selected, False otherwise.
+        """
+        if not filename:
+            # Open a file dialog to select an Inventor document.
+            Tk().withdraw()
+            filename = askopenfilename()
+
+        if not filename:
+            logger.warning("No file selected.")
+            return False
+        else:
+            try:
+                self.app.Documents.Open(filename)
+                logger.info(f"Selected document: {filename}")
+                return True
+            except Exception as e:
+                logger.error(f"Error selecting document '{filename}': {e}")
+                return False
 
 
 # - - - - - - - - - - - - - - - - - - - - -
@@ -67,11 +116,11 @@ if __name__ == "__main__":
         # Create directory for current time.
         os.mkdir(log_path)
         # Set the filename for the log file.
-        config_dict["handlers"]["file"]["filename"] = f"logs/{current_time}/{current_time}_RoverRPi.log"
+        config_dict["handlers"]["file"][
+            "filename"
+        ] = f"logs/{current_time}/{current_time}_RoverRPi.log"
         logging.config.dictConfig(config_dict)
 
     # Create an instance of InventorManager to test the connection.
     manager = InventorManager()
-    logger.info(
-        f"Active Document: {manager.app.ActiveDocument.Name if manager.app.ActiveDocument else 'No active document'}"
-    )
+    manager.select_document()
--- tests/InventorManager_test.py
+++ tests/InventorManager_test.py
@@ -10,6 +10,7 @@ def test_connection():
     manager = InventorManager()
     assert manager.app is not None
 
+
 def test_disconnect():
     """
     Test disconnecting from Inventor application.
@@ -17,3 +18,16 @@ def test_disconnect():
     manager = InventorManager()
     manager.disconnect()
     assert manager.app is None
+
+
+def test_select_document():
+    """
+    Test selecting document method.
+    """
+    manager = InventorManager()
+    assert manager.app is not None
+
+    ret = manager.select_document(
+        r"C:\Users\hnewe\Documents\tbre_automation_tools\example_files\INVENTOR\DOG TOOTH GEARBOX.iam"
+    )
+    assert ret
