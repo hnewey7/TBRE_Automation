daa500bf91c287776d8d73d218247e77f3d67de0
hnewey7 <hnewey7@gmail.com>
2025-06-17 12:54:25 +0100

    fix: bug with unresolved parts
    - add error handling for unresolved parts

--- src/Part.py
+++ src/Part.py
@@ -52,47 +52,59 @@ class Part:
             occurrence: Part occurrence, optional.
             assembly_doc: Assembly document, optional.
         """
-        if occurrence:
-            prop_set = occurrence.Definition.Document.PropertySets.Item(
-                "Design Tracking Properties"
-            )
-        elif assembly_doc:
-            prop_set = assembly_doc.ComponentDefinition.Document.PropertySets.Item(
-                "Design Tracking Properties"
-            )
-        else:
-            logger.error("No occurrence or assembly document provided.")
-            raise ValueError
-
-        # Initial values.
-        self.part_number = prop_set.Item("Part Number").Value
-        self.part_name = prop_set.Item("Description").Value
-
-        # Set reference document.
-        ref_doc = occurrence if occurrence else assembly_doc.ComponentDefinition
-
         try:
-            self.mass = ref_doc.MassProperties.Mass
-            logger.info("Successfully got mass of part.")
+            if occurrence:
+                prop_set = occurrence.Definition.Document.PropertySets.Item(
+                    "Design Tracking Properties"
+                )
+            elif assembly_doc:
+                prop_set = assembly_doc.ComponentDefinition.Document.PropertySets.Item(
+                    "Design Tracking Properties"
+                )
+            else:
+                logger.error("No occurrence or assembly document provided.")
+                raise ValueError
+
+            # Initial values.
+            self.part_number = prop_set.Item("Part Number").Value
+            self.part_name = prop_set.Item("Description").Value
+
+            # Set reference document.
+            ref_doc = occurrence if occurrence else assembly_doc.ComponentDefinition
+
+            try:
+                self.mass = ref_doc.MassProperties.Mass
+                logger.info("Successfully got mass of part.")
+            except Exception as e:
+                self.mass = None
+                logger.error(f"Unable to get mass of part, {e}")
+
+            try:
+                self.x_axis = ref_doc.MassProperties.CenterOfMass.X * 10
+                self.y_axis = ref_doc.MassProperties.CenterOfMass.Y * 10
+                self.z_axis = ref_doc.MassProperties.CenterOfMass.Z * 10
+                logger.info("Successfully got centre of mass of part.")
+
+                self.x_axis_mass = self.x_axis * self.mass
+                self.y_axis_mass = self.y_axis * self.mass
+                self.z_axis_mass = self.z_axis * self.mass
+
+            except Exception as e:
+                self.x_axis = None
+                self.y_axis = None
+                self.z_axis = None
+                self.x_axis_mass = None
+                self.y_axis_mass = None
+                self.z_axis_mass = None
+                logger.error(f"Unable to get centre of mass of part, {e}")
         except Exception as e:
+            logger.error(f"Unable to process part {occurrence.Name}: {e}")
+            self.part_number = occurrence.Name
+            self.part_name = None
             self.mass = None
-            logger.error(f"Unable to get mass of part, {e}")
-
-        try:
-            self.x_axis = ref_doc.MassProperties.CenterOfMass.X * 10
-            self.y_axis = ref_doc.MassProperties.CenterOfMass.Y * 10
-            self.z_axis = ref_doc.MassProperties.CenterOfMass.Z * 10
-            logger.info("Successfully got centre of mass of part.")
-
-            self.x_axis_mass = self.x_axis * self.mass
-            self.y_axis_mass = self.y_axis * self.mass
-            self.z_axis_mass = self.z_axis * self.mass
-
-        except Exception as e:
             self.x_axis = None
             self.y_axis = None
             self.z_axis = None
             self.x_axis_mass = None
             self.y_axis_mass = None
             self.z_axis_mass = None
-            logger.error(f"Unable to get centre of mass of part, {e}")
